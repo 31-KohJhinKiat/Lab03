1f086377ddb1c4eb008d01a66b397a1c65c7166c
29-EmanuelPeh <91875362+29-EmanuelPeh@users.noreply.github.com>
2021-10-06 19:02:19 +0800

    added win lose condition

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     Rigidbody rb;
+
+    private int coin1Count = 4;
+    private int coin2Count = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +20,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (coin1Count == 0)
+        {
+            SceneManager.LoadScene("GamePlay_Level2");
+        }
+        if (coin2Count == 0)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
     }
 
     void FixedUpdate()
@@ -30,14 +41,22 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag.Equals("coin"))
+        if(collision.gameObject.tag.Equals("coin1"))
         {
+            coin1Count--;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag.Equals("coin2"))
+        {
+            coin2Count--;
             Destroy(collision.gameObject);
         }
 
         if (collision.gameObject.tag.Equals("hazardWall"))
         {
             print("game over");
+            SceneManager.LoadScene("LoseScene");
         }
     }
 
