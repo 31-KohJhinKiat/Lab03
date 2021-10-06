85ae64aa645aae5d73b04c6be214f0001c7172b3
JhinKiat <jhinkiat915@gmail.com>
2021-10-06 16:27:28 +0800

    Have done level 1 already.

---
+++ 31_KohJhinKiat_Lab03_v2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    { 
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag.Equals("hazardWall"))
+        {
+            print("game over");
+        }
+    }
+
+}
